1b4d2f61d76c28680f918878e03a634c7f842022
Madhusudhan Konda <m.konda@chocolateminds.com>
2023-03-16 22:26:56 +0000

    initial update

---
+++ app.js
@@ -0,0 +1,9 @@
+const Http = require('http')
+const port = process.env.port || 8080
+const handleRequest = (request, response) => {
+  response.end(`Hello, Azure World`)
+}
+const server = Http.createServer(handleRequest)
+server.listen(port, () => {
+  console.log(`Http server listening on http://localhost:${port}`)
+})
