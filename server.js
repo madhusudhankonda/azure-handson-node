4b78416fc25dcaec5f8d235958326f804939fd66
Madhusudhan Konda <KondaM@ebrd.com>
2023-03-17 16:37:23 +0000

    Message updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const Http = require('http')
 const port = process.env.port || 8080
 const handleRequest = (request, response) => {
-  response.end(`Hello, Azure World!! How are you all!! Enjoy`)
+  response.end(`Hello, Azure World!! How are you all!!`)
 }
 const server = Http.createServer(handleRequest)
 
